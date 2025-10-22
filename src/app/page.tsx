05d4db40b7faece94df7bd7d17f1f0d86206e3e6
duasosandy <sansd.dev@gmail.com>
2025-10-22 09:41:43 +0800

    fix some design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ import { Book } from "@/components/Book";
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
-      <header className="sticky top-0 z-10 bg-white/80 backdrop-blur-sm h-16 flex justify-between items-center border-b shadow-sm px-4">
-        <h2 className="text-xl font-semibold text-primary">Books</h2>
-      </header>
       <main className="flex-1 p-8">
         <Content />
       </main>
